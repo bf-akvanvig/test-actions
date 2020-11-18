4a15bb1ac317a2b8251782ad72c4a0fe67c9dbc4
Anders Kvanvig <74308646+bf-akvanvig@users.noreply.github.com>
2020-11-18 14:50:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,14 +8,13 @@ terraform {
   }
 }
 
-provider "libvirt" {
-  uri = "qemu:///system"
-}
-
 #provider "libvirt" {
-#  alias = "server2"
-#  uri   = "qemu+ssh://root@192.168.100.10/system"
+#  uri = "qemu:///system"
 #}
+
+provider "libvirt" {
+  uri   = "qemu+ssh://spc-akvanvig-lab-lb7000.p1.lab.basefarm.net:7000/system"
+}
 
 resource "libvirt_volume" "centos7-qcow2" {
   name   = "centos7.qcow2"
